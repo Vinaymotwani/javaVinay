fe271ed58bda113271b2f3a39fcb5a0e772ffa2e
Vinaymotwani <2024.vinay.motwani@ves.ac.in>
2025-10-15 20:23:26 +0530

    Upedat Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-class Main{
+public class Main{
     public static void main(String[] args){
 
         System.out.println("My name is Vinu");
         System.out.println("I m in devops lec");
     }
+
 }
