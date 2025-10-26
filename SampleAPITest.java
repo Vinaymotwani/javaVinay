c4fe61760382fd9dd112ab0fe69e2a34a80ec623
Vinaymotwani <2024.vinay.motwani@ves.ac.in>
2025-10-26 18:36:05 +0530

    Update SampleAPITest.java

--- SampleAPITest.java
+++ SampleAPITest.java
@@ -1,24 +1,31 @@
-package com.apitests;
+
+
+package com.api.automation; // <-- THIS IS THE FIX
+
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
+
+// The class name "SimpleApiTest" MUST match the file name "SimpleApiTest.java"
 public class SimpleApiTest {
-// A simple, reliable API test using REST-Assured
-@Test
-public void verifyGitHubStatusIs200() {
-given()
-.when()
-.get("https://api.github.com/users/octocat")
-.then()
-.statusCode(200); // Asserts the response code is 200 (Success)
-}
-// You can add more tests here, like a failure example
-@Test
-public void verifyInvalidEndpointStatusIs404() {
-given()
-.when()
-.get("https://api.github.com/nonexistent")
-.then()
-.statusCode(404); // Asserts the response code is 404 (Not Found)
+    
+    // A simple, reliable API test using REST-Assured
+    @Test
+    public void verifyGitHubStatusIs200() {
+        given()
+        .when()
+            .get("https://api.github.com/users/octocat")
+        .then()
+            .statusCode(200); // Asserts the response code is 200 (Success)
+    }
+
+    // You can add more tests here, like a failure example
+    @Test
+    public void verifyInvalidEndpointStatusIs404() {
+        given()
+        .when()
+            .get("https://api.github.com/nonexistent")
+        .then()
+            .statusCode(404); // Asserts the response code is 404 (Not Found)
+    }
 }
 
-}
